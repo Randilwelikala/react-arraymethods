c43a7fd5fd4ffd59ff209db371ace8191bf1b061
randilwelikala <randilgimantha646@gmail.com>
2025-02-17 22:21:10 +0530

    add display array item on the screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
+import ArrayMap from './components/ArrayMap';
 import './App.css';
+
+const names = [
+  "Alice",
+  "BOb",
+  "Charlie",
+  "David",
+  "Eve",
+  "Frank",
+  "Grace",
+  "Hannah",
+  "Ivy",
+  "Jack"
+];
+
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <ArrayMap nameList={names}/>        
       </header>
     </div>
   );
---
+++ src/components/ArrayMap.jsx
@@ -0,0 +1,18 @@
+const ArrayMap = ({nameList}) =>{
+    return(
+        <div>
+            <h1>Full Name List(Map)</h1>
+            <ul>
+                {nameList.map((name,index)=>{
+                        return(
+                            <li key={index}>{name}</li>
+                        )
+                    })
+                }
+            </ul>
+
+        </div>
+    );
+};
+
+export default ArrayMap;

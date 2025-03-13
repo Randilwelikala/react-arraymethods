f79883ee2a76ec5584e613f0e4b3e420acf70bc5
randilwelikala <randilgimantha646@gmail.com>
2025-03-13 22:48:52 +0530

    add item to a array with delete some items using arraysplice method

--- src/components/ArraySplice.jsx
+++ src/components/ArraySplice.jsx
@@ -1,7 +1,9 @@
 const ArraySplice = ({nameList}) => {
     var arr1=[...nameList];
+    var arr2=[...nameList];
 
-    arr1.splice(3,0,"Test");
+    arr1.splice(3,0,"Between Charlie and David");
+    arr2.splice(3,2,"Test");
     // const arr2 = nameList.slice(3,nameList.length);
 
 
@@ -25,6 +27,18 @@ const ArraySplice = ({nameList}) => {
             <table border={1} cellPadding={5} >
 
                 {arr1.map((name,index)=>(
+                    <tr>
+                        <td key={index}>{index}</td>
+                        <td key={name}>{name}</td>
+                    </tr>
+                ))}
+            </table>
+
+            <h2>Array 02</h2>
+
+            <table border={1} cellPadding={5} >
+
+                {arr2.map((name,index)=>(
                     <tr>
                         <td key={index}>{index}</td>
                         <td key={name}>{name}</td>
